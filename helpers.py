4131bf4a30ec2252a4bf4a5a37fc3318daf58846
canadien91 <nefedov.alexander91@yandex.ru>
2014-05-16 16:09:07 +0400

    Delete background.png

--- helpers.py
+++ helpers.py
@@ -12,13 +12,13 @@ def genLabelText( text, i, left=True ):
     label["fg"]     = ( 0.148, 0.176, 0.105, 1 )
     label["align"]  = TextNode.ALeft
     label["scale"]  = 0.06
-    label["font"]   = loader.loadFont('fonts/FreeMonoBold.ttf')
+    label["font"]   = loader.loadFont('data/fonts/FreeMonoBold.ttf')
     if left:    label["pos"] = ( -1.3, .95-.06*i )
     else:       label["pos"] = ( 0.95, .95-.06*i )
     return label
 
 def loadObject( tex=None, pos=Point2(0,0), depth=SPRITE_POS, scale=1, transparency=True ):
-    obj = loader.loadModel( "models/plane" )
+    obj = loader.loadModel( "data/models/plane" )
     obj.reparentTo( camera )
     obj.setPos( Point3( pos.getX( ), depth, pos.getY( ) ) )
     obj.setScale( scale )
@@ -26,6 +26,6 @@ def loadObject( tex=None, pos=Point2(0,0), depth=SPRITE_POS, scale=1, transparen
     obj.setDepthTest( False )
     if transparency: obj.setTransparency( 1 )
     if tex:
-        tex = loader.loadTexture("sprites/"+tex+".png") 
+        tex = loader.loadTexture("data/sprites/"+tex+".png") 
         obj.setTexture(tex, 1)
     return obj
